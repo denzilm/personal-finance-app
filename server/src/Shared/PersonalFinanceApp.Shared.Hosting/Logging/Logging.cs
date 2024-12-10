f67ec4d3731ffdeb59c0be3fdf1674f4e02192d5
Denzil Martin <denzil.martin07@gmail.com>
2024-12-10 22:32:02 +1300

    Adding logging support

---
+++ server/src/Shared/PersonalFinanceApp.Shared.Hosting/Logging/Logging.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Sinks.SystemConsole.Themes;
+
+namespace PersonalFinanceApp.Shared.Hosting.Logging;
+
+public static class Logging
+{
+    public static void Setup(IHostEnvironment environment, IConfiguration configuration)
+    {
+        if (environment.IsDevelopment())
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .Enrich.WithExceptionDetails()
+                .Enrich.WithMachineName()
+                .Enrich.WithProcessId()
+                .Enrich.WithProcessName()
+                .Enrich.WithMemoryUsage()
+                .WriteTo.Console(outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Level:u3} {MemoryUsage} - {Message:lj}{NewLine}{Exception}", theme: AnsiConsoleTheme.Code)
+                .WriteTo.Seq(serverUrl: configuration.GetConnectionString("Seq")!)
+                .CreateLogger();
+        }
+        else
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .Enrich.WithExceptionDetails()
+                .Enrich.WithMachineName()
+                .Enrich.WithProcessId()
+                .Enrich.WithProcessName()
+                .Enrich.WithMemoryUsage()
+                .WriteTo.Seq(serverUrl: configuration.GetConnectionString("Seq")!)
+                .CreateLogger();
+        }
+    }
+}
